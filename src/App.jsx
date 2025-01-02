483ae0cd186c914d3aa96d7398a9f9e3290f242b
Ming Jui Tsai <mingjuitsai@Mings-MacBook-Pro.local>
2025-01-02 16:40:09 +1100

    Build grids

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,17 @@ const App = () => {
       <main className="container mx-auto px-4 py-6 bg-gray-50 bg-gray-50">
         <h1 className="text-2xl font-bold mb-6">550 Hotels Available in Melbourne</h1>
 
+        <div className="grid md:grid-cols-3 gap-4">
+          <div className="container px-4 py-2 bg-gray-200">
+            02
+          </div>
+          <div className="container px-4 py-2 col-span-1 md:col-span-2">
+            03
+          </div>
+        </div>
       </main>
+
+
     </div>
   );
 };
